5978cfc97c61f3a11b186a22e6f6f5f407e2a5a7
m-eminozdogan <m.eminozdogan@hotmail.com>
2022-03-27 12:31:58 +0300

    page routes done

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,40 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 function App() {
+  const user = true;
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/register" />}
+          />
+
+          <Route
+            path="/register"
+            element={!user ? <Register /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
+          {user && (
+            <>
+              <Route path="/series" element={<Home type="series" />} />
+              <Route path="/movies" element={<Home type="movies" />} />
+              <Route path="/watch" element={<Watch />} />
+            </>
+          )}
+        </Routes>
+      </Router>
     </div>
   );
 }
--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import "./register.scss";
-
+import { Navigate } from "react-router-dom";
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -22,7 +22,15 @@ export default function Register() {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
           />
-          <button className="loginButton">Sign In</button>
+
+          <button
+            className="loginButton"
+            onClick={() => {
+              <Navigate to="/login" />;
+            }}
+          >
+            Sign In
+          </button>
         </div>
       </div>
       <div className="container">
